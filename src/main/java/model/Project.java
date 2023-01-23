402656dba9dc12e1d267b00591e0e27c38b25a15
bruno <bruno@LAPTOP-CCT3QMHG>
2023-01-23 17:30:10 -0300

    Criando as Classes

---
+++ src/main/java/model/Project.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.sql.Date;
+
+/**
+
+ @author bruno
+ */
+public class Project {
+    
+    private int id;
+    private String name;
+    private String description;
+    private Date createdAt;
+    private Date updatedAt;
+
+    public Project(int id, String name, String description, Date createdAt, Date updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+}
