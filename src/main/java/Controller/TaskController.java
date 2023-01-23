aee6298539315d31a726309d0748aad1cb6dd446
bruno <bruno@LAPTOP-CCT3QMHG>
2023-01-23 20:45:32 -0300

    Criando Controladores

--- src/main/java/Controller/TaskController.java
+++ src/main/java/Controller/TaskController.java
@@ -4,10 +4,49 @@
  */
 package Controller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+import model.Task;
+import util.ConnectionFactory;
+
 /**
 
  @author bruno
  */
 public class TaskController {
-    
+    public void save (Task task) {
+       String sql = "INSERT INTO tasks (idProject, name, description, completed, notes, deadline, createdAt, updatedAt) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+       Connection connection = null;
+       PreparedStatement statement = null;
+    }
+
+    public void update(Task task) {
+
+    }  
+
+    public void removeById(int taskId) throws SQLException{
+
+        String sql = "DELETE FROM tasks WHERE id = ?";
+
+         Connection conn = null;
+         PreparedStatement statement =  null;
+
+         try {
+            conn = ConnectionFactory.getConnection ();
+            statement = conn.prepareStatement ( sql );
+            statement.setInt ( 1, taskId);
+            statement.execute ();
+        } catch (SQLException e ) {
+            throw new SQLException ("Erro ao deletar tarefa");
+        } finally {
+            ConnectionFactory.closeConnection (conn);
+        }
+    }
+
+    public List<Taks> getAll (int idProject){
+        return null;
+    } 
 }
